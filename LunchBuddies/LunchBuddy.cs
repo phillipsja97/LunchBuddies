0ca10008fc1f64bb4bd9f9c225f885ee8425d0d6
Jamie <phillipsja97@gmail.com>
2020-03-13 18:05:15 -0500

    Added methods to return the console outputs needed. assignment finished.

---
+++ LunchBuddies/LunchBuddy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+    {
+    class LunchBuddy
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        List<string> Buddies = new List<string>();
+        public LunchBuddy(string first, string last)
+        {
+        FirstName = first;
+        LastName = last;
+        Buddies.Add($"Filip Forsberg");
+        Buddies.Add("Juuse Sarros");
+        Buddies.Add("Ryan Johannson");
+        }
+
+        public void Eating()
+        {
+            var restaurant = new Restaurants();
+            var location = restaurant.RandomRestaurant();
+            Console.WriteLine($"{FirstName} {LastName} is eating at {location}.");
+        }
+
+        public void eat(string food)
+        {
+            Console.WriteLine($"{FirstName} {LastName} ate {food} at the office today.");
+        }
+
+        public void Company()
+        {
+            string friends = string.Join(",", Buddies.ToArray());
+            var restaurant = new Restaurants();
+            var location = restaurant.RandomRestaurant();
+            Console.WriteLine($"{FirstName} is eating at {location} with {friends}.");
+        }
+
+        public void WhatToEat(string food)
+        {
+            string friends = string.Join(",", Buddies.ToArray());
+            var restaurant = new Restaurants();
+            var location = restaurant.RandomRestaurant();
+            Console.WriteLine($"{FirstName} is eating {food} at {location} with {friends}");
+        }
+            
+
+    }
+}
---
+++ LunchBuddies/Restaurants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchBuddies
+{
+    class Restaurants
+    {
+        List<string> restaurantGroup = new List<string>();
+        public Restaurants()
+        {
+            restaurantGroup.Add("Husk");
+            restaurantGroup.Add("Waffle House");
+            restaurantGroup.Add("Taco Bell");
+            restaurantGroup.Add("Lockeland Table");
+            restaurantGroup.Add("Whataburger");
+        }
+
+        public string RandomRestaurant()
+        {
+            var random = new Random();
+            var list = restaurantGroup;
+            int index = random.Next(list.Count);
+            return list[index];
+        }
+    }
+}
